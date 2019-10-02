aa24271e2f03086a951b71835a8dd34827552332
Manju KC <kcmanjuthapa@gmail.com>
2019-10-02 17:32:10 -0400

    imported CoinList into the Coins file

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,1 +1,16 @@
 import React from "react";
+import CoinList from "./CoinList";
+
+class Coins extends Component {
+
+    
+  render() {
+    return(
+        <div>
+            
+        </div>
+    )
+  }
+}
+
+export default Coins;
