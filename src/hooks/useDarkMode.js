5b42eb179901e667ffad4d08ae75a33a8cc05bf3
Manju KC <kcmanjuthapa@gmail.com>
2019-10-02 15:29:05 -0400

    setup the useEffect

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,4 +4,7 @@ import { useLocalStorage } from './useLocalStorage.js';
 export const useDarkMode = (key) => {
     const [darkMode, setDarkMode] = useLocalStorage(key);
 
+    useEffect(() => {
+        
+    })
 }
