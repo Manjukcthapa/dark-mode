5f6795787a58412aa68a66d58483beeae170ccf5
Manju KC <kcmanjuthapa@gmail.com>
2019-10-02 15:35:46 -0400

    added if else statement logic

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -5,6 +5,6 @@ export const useDarkMode = (key) => {
     const [darkMode, setDarkMode] = useLocalStorage(key);
 
     useEffect(() => {
-        
+        darkMode?document.querySelector('body').classList.add('dark-mode'):document.querySelector('body').classList.remove('dark-mode')
     })
 }
